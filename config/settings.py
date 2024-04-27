b97c9c6aa5677a2e6e06b42f484a77f0748546d1
Anton Shpakovych <shpakovych18@gmail.com>
2024-04-27 15:14:39 +0300

    Initialized django project, added setting variants: local, shared

---
+++ config/settings.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DEBUG = os.getenv("DJANGO_DEBUG_MODE") == "True"
+
+if DEBUG:
+    from .local import *
+else:
+    # from .production import *
+    pass
