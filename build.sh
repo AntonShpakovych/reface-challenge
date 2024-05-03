a0643df866a981d90ea9faf60e421171911fa74b
Anton Shpakovych <shpakovych18@gmail.com>
2024-05-03 13:16:28 +0300

    Drop creation db functions command

--- build.sh
+++ build.sh
@@ -7,4 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-psql PGPASSWORD="$DATABASE_PASSWORD" psql -h "$DATABASE_HOST" -U "$DATABASE_USER" "$DATABASE_NAME" -W -f ./count_words.sql
