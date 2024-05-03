1a028c4b6ce2274065b3a030d703f4351f347be4
Anton Shpakovych <shpakovych18@gmail.com>
2024-05-03 12:49:05 +0300

    Create DB functions on prod

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+psql -d cornelius -U pepipugb  -W -f ./count_unique_words.sql
+psql -d cornelius -U pepipugb  -W -f ./count_words.sql
